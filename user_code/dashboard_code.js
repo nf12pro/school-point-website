37fd88140fb9b30704bcd5246481b3db339198bf
nf12pro <rayofaik@gmail.com>
2026-02-12 14:54:22 +0100

    Day 1 Log Off

--- user_code/dashboard_code.js
+++ user_code/dashboard_code.js
@@ -4,16 +4,18 @@ let purchase_id = 0;
 document.getElementById('button_purchase1').addEventListener('click', function() {
     if (confirm('Are you sure you want to make this purchase?')) {
         purchase_id = 1; 
+        points -= 50;
         document.getElementById('points').innerText = 'Points: ' + points;
-        document.getElementById('response').innerText = 'Correct! +10 points. Total: ' + points;
+        document.getElementById('response').innerText = 'Your Teacher Has Received it';
     }
 });
 
 document.getElementById('button_purchase2').addEventListener('click', function() {
     if (confirm('Are you sure you want to make this purchase?')) {
         purchase_id = 2; 
+        points -= 50;
         document.getElementById('points').innerText = 'Points: ' + points;
-        document.getElementById('response').innerText = 'Correct! +10 points. Total: ' + points;
+        document.getElementById('response').innerText = 'Thank you for your purchase!';
     }
 });
 
